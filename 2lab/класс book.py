eeff9fea60055ac65fb5e939b26f74bc375d8f26
Apypet <nastya.yagin@gmail.com>
2025-02-20 20:47:24 +0300

    2lab

---
+++ 2lab/класс book.py
@@ -0,0 +1,36 @@
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+
+# TODO написать класс Book
+class Book:
+    def __init__(self, id_, name, pages):
+        self.id = id_ # Инициализация идентификатора книги
+        self.name = name # Инициализация названия книги
+        self.pages = pages # Инициализация количества страниц
+
+    def __str__(self):
+        return f'Книга "{self.name}"'
+
+    def __repr__(self):
+        return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
+
+if __name__ == '__main__':
+    # инициализируем список книг, создаем список объектов Book на основе данных из BOOKS_DATABASE
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    for book in list_books:
+        print(book)  # проверяем метод __str__
+
+    print(list_books)  # проверяем метод __repr__
